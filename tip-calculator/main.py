2af631beb869df50d9f620df605b612c4be3c836
iota07 <hanesananhouvannasak@googlemail.com>
2024-01-06 21:33:39 +0100

    add back

--- tip-calculator/main.py
+++ tip-calculator/main.py
@@ -1,7 +1,7 @@
 print("Welcome to the tip calculator!")
-bill = input("What is the bill total? in €:\n")
-people = input("how man people that bill need to be divided to?\n")
-tip = input("Enter the tip percentage:\n")
+bill = input("What is the bill total? in €: ")
+people = input("how man people that bill need to be divided to? ")
+tip = input("Enter the tip percentage: ")
 
 splitted_bill = float(bill) / int(people)
 pay_per_person = splitted_bill * (1 + float(tip) / 100)
