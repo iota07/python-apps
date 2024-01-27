b59d7b72e1dcfa6116cc682ee061c1a4f06a2dbd
iota07 <hanesananhouvannasak@googlemail.com>
2024-01-27 12:27:58 +0100

    finish higher&lower

--- Higher-or-Lower/main.py
+++ Higher-or-Lower/main.py
@@ -9,28 +9,45 @@ PLAYER_POINTS = 0
 still_going = True
 
 first_random = random.choice(data)
+second_random = random.choice(data)
 
 desired_keys = ["name", "description", "country"]
 
-for key in desired_keys:
-    value = first_random.get(key)
-    print(f"    {key}: {value}")
+remaining = first_random
 
-second_random = random.choice(data)
-for key in desired_keys:
-    value = second_random.get(key)
-    print(f"    {key}: {value}")
+while still_going:
+    if remaining == second_random:
+        second_random = random.choice(data)
 
-# while still_going:
-#     print(remaining)
-#     newer = print(random.choice(data).key[0, 2, 3])
-#     guess = print("who has the most follower? A or B?")
-#     print(remaining.key[0, 2, 3])
-#     print(newer.key[0, 2, 3])
-#     if remaining.key[1] > newer.key[1]:
-#         answer = "A"
-#     elif first_random.key[1] < newer.key[1]:
-#         answer = "B"
-#         remaining = newer
-#     if guess == answer:
-#         print("correct")
+    for key in desired_keys:
+        value = remaining.get(key)
+        print(f"    {key}: {value}")
+    print("\n & \n")
+
+    for key in desired_keys:
+        value = second_random.get(key)
+        print(f"    {key}: {value}")
+    print("\n\n")
+
+    guess = input("who has the most follower? A or B?: ").lower()
+    print("\n")
+    answer = ""
+
+    if int(remaining.get("follower_count")) > int(second_random.get("follower_count")):
+        answer = "a"
+        second_random = random.choice(data)
+
+    elif int(remaining.get("follower_count")) < int(
+        second_random.get("follower_count")
+    ):
+        answer = "b"
+        remaining = second_random
+        second_random = random.choice(data)
+
+    if guess == answer:
+        print("Correct!")
+        print("\nNEXT ONE:\n")
+        PLAYER_POINTS += 1
+    else:
+        print(f"\nGAME OVER! You got {PLAYER_POINTS} points")
+        still_going = False
